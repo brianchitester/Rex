bde08f203b6392a6098d0f7038d43b169e0e4ce6
Brian Chitester <brianchitester@gmail.com>
2022-08-05 12:58:08 -0400

    rough recs on the client

--- src/pages/TrackDetails.tsx
+++ src/pages/TrackDetails.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTrackNftsOwnersQuery, useTrackQuery } from "@spinamp/spinamp-hooks";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import {
+  fetchCollectionForAddress,
+  fetchTrackById,
+  ITrack,
+} from "@spinamp/spinamp-sdk";
 import useWindowDimensions from "../hooks/useWindowDimensions";
+import Track from "../components/lib/Track";
 
 function TrackDetails() {
+  const [recs, setRecs] = useState<{ track: ITrack; count: number }[]>([]);
+  const [calledOnce, setCalledOnce] = useState(false);
   const params = useParams();
   const navigate = useNavigate();
   const { width, height } = useWindowDimensions();
@@ -32,6 +40,51 @@ function TrackDetails() {
   const filteredOwners = owners.filter((owner) => !!owner);
 
   const imageSize = height < width ? height * 0.6 : width * 0.6;
+
+  // madness ahead
+  const getRecs = async () => {
+    // array of arrays
+    const ownertracks = await Promise.all(
+      filteredOwners.map(async (owner) => {
+        // this is going to get called a lot for certain tracks
+        const ownerCollection = await fetchCollectionForAddress(owner);
+        const tracks = ownerCollection.map((track) => track.id);
+        return tracks;
+      })
+    );
+
+    const recsObj = ownertracks.reduce(
+      (accum: { [key: string]: number }, tracks) => {
+        tracks.forEach((track) => {
+          if (!accum[track]) {
+            accum[track] = 1;
+          } else {
+            accum[track] = accum[track] + 1;
+          }
+        });
+        return accum;
+      },
+      {}
+    );
+    const recsArr = await Promise.all(
+      Object.keys(recsObj).map(async (key) => {
+        const track = await fetchTrackById(key);
+        return {
+          track: track ?? ({} as ITrack),
+          count: recsObj[key],
+        };
+      })
+    );
+    recsArr.sort((a, b) => {
+      return b.count - a.count;
+    });
+    setCalledOnce(true);
+    setRecs(recsArr.slice(1, 5)); // first rec will be the same track
+  };
+
+  if (!calledOnce) {
+    getRecs();
+  }
 
   return (
     <StyledTrackDetailsContainer>
@@ -67,6 +120,20 @@ function TrackDetails() {
           No owners,{" "}
           <BuyButton href={track?.websiteUrl}>be the first!</BuyButton>
         </Owners>
+      )}
+      {recs.length > 0 && (
+        <>
+          <div>Similar taste</div>
+          {recs.map((rec) => {
+            return (
+              <Track
+                onClick={() => navigate(`/trackDetails/${rec.track.id}`)}
+                key={rec.track.id}
+                track={rec.track}
+              />
+            );
+          })}
+        </>
       )}
     </StyledTrackDetailsContainer>
   );
