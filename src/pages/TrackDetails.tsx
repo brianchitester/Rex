af070a2cb8173ce24e0208eb33f266b994a6098d
Brian Chitester <brian.chitester@coinbase.com>
2022-08-04 22:31:13 -0400

    better styling on track details

--- src/pages/TrackDetails.tsx
+++ src/pages/TrackDetails.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useTrackNftsOwnersQuery, useTrackQuery } from "@spinamp/spinamp-hooks";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 function TrackDetails() {
   const params = useParams();
   const navigate = useNavigate();
+  const { width } = useWindowDimensions();
   const trackId = `${params?.chain ?? ""}/${params?.token ?? ""}${
     params.id ? `/${params.id}` : ""
   }`;
@@ -29,45 +31,81 @@ function TrackDetails() {
 
   const filteredOwners = owners.filter((owner) => !!owner);
 
+  const imageSize = width * 0.6;
+
   return (
     <StyledTrackDetailsContainer>
-      <div>
-        <div>Welcome TrackDetails</div>
-        <div>{track?.title}</div>
-        <div>{track?.artist.name}</div>
-        <div>{track?.description}</div>
-        <img
-          alt={track?.title}
-          width={150}
-          height={150}
-          src={track?.lossyArtworkUrl}
-        />
-        <audio controls src={track?.lossyAudioUrl}>
-          Your browser does not support the
-          <code>audio</code> element.
-        </audio>
-        {filteredOwners.length > 0 ? (
-          <>
-            <div>Owners</div>
-            <ul>
-              {filteredOwners.map((owner) => (
-                <li key={owner} onClick={() => navigate(`/owner/${owner}`)}>
-                  {owner}
-                </li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          <div>No owners, be the first!</div>
-        )}
-      </div>
+      <StyledTrackCover
+        alt={track?.title}
+        width={imageSize}
+        height={imageSize}
+        src={track?.lossyArtworkUrl}
+      />
+      <TrackTitle>{track?.title}</TrackTitle>
+      <TrackArtist>{track?.artist.name}</TrackArtist>
+      <TrackDescription>
+        {track?.description?.replace(/<[^>]*>?/gm, "")}
+      </TrackDescription>
+
+      <audio controls src={track?.lossyAudioUrl}>
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
+      {filteredOwners.length > 0 ? (
+        <Owners>
+          <h3>Owners</h3>
+          {filteredOwners.map((owner) => (
+            <OwnerLink key={owner} onClick={() => navigate(`/owner/${owner}`)}>
+              {owner}
+            </OwnerLink>
+          ))}
+        </Owners>
+      ) : track?.platformId === "nina" ? (
+        <Owners>{track.platformId} support coming soon!</Owners>
+      ) : (
+        <Owners>
+          No owners,{" "}
+          <BuyButton href={track?.websiteUrl}>be the first!</BuyButton>
+        </Owners>
+      )}
     </StyledTrackDetailsContainer>
   );
 }
 
 const StyledTrackDetailsContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledTrackCover = styled.img`
+  padding: 20px;
+`;
+
+const BuyButton = styled.a`
+  cursor: pointer;
+`;
+
+const TrackTitle = styled.h1`
+  margin: 0px;
+`;
+
+const TrackArtist = styled.h2`
+  margin: 0px;
+`;
+
+const TrackDescription = styled.div`
+  margin: 1em;
+`;
+
+const Owners = styled.div`
+  margin-top: 1em;
+`;
+
+const OwnerLink = styled.div`
+  cursor: pointer;
+  text-overflow: ellipsis;
+  margin: 0.5em 0em;
 `;
 
 export default TrackDetails;
