904aa543421eddcc9a413389f1792c39f6472036
Brian Chitester <brian.chitester@coinbase.com>
2022-08-06 18:57:44 -0400

    fix build

--- src/pages/TrackList.tsx
+++ src/pages/TrackList.tsx
@@ -7,71 +7,72 @@ import useDraggableScroll from "use-draggable-scroll";
 import { Box } from "@mui/material";
 
 function TrackList() {
-    const { tracks, isLoading, isError } = usePaginatedTracksQuery(50);
-    const navigate = useNavigate();
-    const ref =
-        useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
-    const { onMouseDown } = useDraggableScroll(ref, { direction: "vertical" });
+  const { tracks, isLoading, isError } = usePaginatedTracksQuery(50);
+  const navigate = useNavigate();
+  const ref =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { onMouseDown } = useDraggableScroll(ref, { direction: "vertical" });
 
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    let [offsetTop, setOffsetTop] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  let [offsetTop, setOffsetTop] = useState(0);
 
-    const handleScroll = (e: any) => {
-        let midPoint = e.target.offsetHeight / 2;
-        setSelectedIndex(Math.floor((midPoint + e.target.scrollTop) / 50) - 1);
+  const handleScroll = (e: any) => {
+    let midPoint = e.target.offsetHeight / 2;
+    setSelectedIndex(Math.floor((midPoint + e.target.scrollTop) / 50) - 1);
+  };
+
+  useEffect(() => {
+    ref.current?.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    setOffsetTop(ref.current?.getBoundingClientRect().top);
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ref.current?.removeEventListener("scroll", handleScroll);
     };
+  }, [isLoading]);
 
-    useEffect(() => {
-        ref.current?.addEventListener("scroll", handleScroll, {
-            passive: true
-        });
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
 
-        setOffsetTop(ref.current?.getBoundingClientRect().top);
-
-        return () => {
-            ref.current?.removeEventListener("scroll", handleScroll);
-        };
-    }, [isLoading]);
-
-    if (isLoading) {
-        return <p>Loading</p>;
-    }
-
-    if (isError) {
-        return (
-            <div>
-                <p>Ups! Something went wrong</p>
-            </div>
-        );
-    }
-    offsetTop = offsetTop || 0;
-
+  if (isError) {
     return (
-        <div>
-            <TrackListContainer
-                ref={ref}
-                onMouseDown={onMouseDown}
-                style={{ height: `calc(100vh - ${offsetTop}px)` }}
-            >
-                {tracks.map((track, index) => (
-                    <Track
-                        expanded={selectedIndex === index}
-                        onClick={() => navigate(`/trackDetails/${track.id}`)}
-                        key={track.id}
-                        track={track}
-                    />
-                ))}
-                <Box sx={{ height: "150px" }} />
-            </TrackListContainer>
-        </div>
+      <div>
+        <p>Ups! Something went wrong</p>
+      </div>
     );
+  }
+  offsetTop = offsetTop || 0;
+
+  return (
+    <div>
+      <TrackListContainer
+        ref={ref}
+        onMouseDown={onMouseDown}
+        style={{ height: `calc(100vh - ${offsetTop}px)` }}
+      >
+        {tracks.map((track, index) => (
+          <Track
+            expanded={selectedIndex === index}
+            onClick={() => navigate(`/trackDetails/${track.id}`)}
+            key={track.id}
+            track={track}
+          />
+        ))}
+        <Box sx={{ height: "150px" }} />
+      </TrackListContainer>
+    </div>
+  );
 }
 
 const TrackListContainer = styled.div`
-    overflow-y: scroll;
-    overflow-x: hidden;
-    background: black;
-    height: 300px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background: black;
+  height: 300px;
 `;
 
 export default TrackList;
