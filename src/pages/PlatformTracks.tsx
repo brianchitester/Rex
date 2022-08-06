67538f0d233710dd11895d1030f8ad98f2b3c4bb
Gal Shenar <gshenar@gmail.com>
2022-08-06 18:46:18 -0400

    track pages

--- src/pages/PlatformTracks.tsx
+++ src/pages/PlatformTracks.tsx
@@ -1,50 +1,81 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { usePaginatedTracksQuery } from "@spinamp/spinamp-hooks";
 import { useNavigate, useParams } from "react-router-dom";
 import Track from "../components/lib/Track";
 import styled from "styled-components";
+import { Box } from "@mui/material";
+import useDraggableScroll from "use-draggable-scroll";
 
 function PlatformTracks() {
-  const params = useParams();
-  const { tracks, isLoading, isError } = usePaginatedTracksQuery(50, {
-    filter: {
-      platformId: { in: [params?.platformId ?? ""] },
-    },
-  });
-  const navigate = useNavigate();
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
+    const params = useParams();
+    const { tracks, isLoading, isError } = usePaginatedTracksQuery(50, {
+        filter: {
+            platformId: { in: [params?.platformId ?? ""] }
+        }
+    });
+    const navigate = useNavigate();
+    const ref =
+        useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+    const { onMouseDown } = useDraggableScroll(ref, { direction: "vertical" });
 
-  if (isError) {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    let [offsetTop, setOffsetTop] = useState(0);
+
+    const handleScroll = (e: any) => {
+        let midPoint = e.target.offsetHeight / 2;
+        setSelectedIndex(Math.floor((midPoint + e.target.scrollTop) / 50) - 1);
+    };
+
+    useEffect(() => {
+        ref.current?.addEventListener("scroll", handleScroll, {
+            passive: true
+        });
+
+        setOffsetTop(ref.current?.getBoundingClientRect().top);
+
+        return () => {
+            ref.current?.removeEventListener("scroll", handleScroll);
+        };
+    }, [isLoading]);
+
+    if (isLoading) {
+        return <p>Loading</p>;
+    }
+
+    if (isError) {
+        return (
+            <div>
+                <p>Ups! Something went wrong</p>
+            </div>
+        );
+    }
+    offsetTop = offsetTop || 0;
+
     return (
-      <div>
-        <p>Ups! Something went wrong</p>
-      </div>
+        <div>
+            <TrackListContainer
+                ref={ref}
+                onMouseDown={onMouseDown}
+                style={{ height: `calc(100vh - ${offsetTop}px)` }}
+            >
+                {tracks.map((track, index) => (
+                    <Track
+                        expanded={selectedIndex === index}
+                        onClick={() => navigate(`/trackDetails/${track.id}`)}
+                        key={track.id}
+                        track={track}
+                    />
+                ))}
+                <Box sx={{ height: "150px" }} />
+            </TrackListContainer>
+        </div>
     );
-  }
-
-  return (
-    <div>
-      <TrackListTitle>{params?.platformId}</TrackListTitle>
-      <div>
-        {tracks.map((track) => (
-          <Track
-            onClick={() => navigate(`/trackDetails/${track.id}`)}
-            key={track.id}
-            track={track}
-          />
-        ))}
-      </div>
-    </div>
-  );
 }
-
-const TrackListTitle = styled.h1`
-  font-family: permanent-marker, sans-serif;
-  font-weight: 400;
-  font-style: normal;
-  margin: 1em;
+const TrackListContainer = styled.div`
+    overflow-y: scroll;
+    overflow-x: hidden;
+    background: black;
+    height: 300px;
 `;
 
 export default PlatformTracks;
