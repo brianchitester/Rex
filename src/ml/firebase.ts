2c26b859ed657f2bdc8f23af0bfe0b0c89bea5a6
Brian Chitester <brian.chitester@coinbase.com>
2022-08-07 12:54:59 -0400

    fix favs

--- src/ml/firebase.ts
+++ src/ml/firebase.ts
@@ -1,71 +1,92 @@
+import { ConstructionOutlined } from "@mui/icons-material";
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, getDoc, getDocs, collection, arrayUnion, updateDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  arrayUnion,
+  updateDoc,
+} from "firebase/firestore";
 
-const classificationsCollection = "track-classifications-2"
+const classificationsCollection = "track-classifications-2";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDuTnq5_CJJR5EQszLhkraoPjxcqdGUxKE",
-    authDomain: "meta-rex.firebaseapp.com",
-    projectId: "meta-rex",
-    storageBucket: "meta-rex.appspot.com",
-    messagingSenderId: "243124280516",
-    appId: "1:243124280516:web:5b85347afc5d6f23ec3d91"
+  apiKey: "AIzaSyDuTnq5_CJJR5EQszLhkraoPjxcqdGUxKE",
+  authDomain: "meta-rex.firebaseapp.com",
+  projectId: "meta-rex",
+  storageBucket: "meta-rex.appspot.com",
+  messagingSenderId: "243124280516",
+  appId: "1:243124280516:web:5b85347afc5d6f23ec3d91",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
-export const storeTrackClassifications = async (trackId: string, trackMetadata: object) => {
-    console.log("Storing in firebase... " + trackId)
-    // Add a new document in collection "cities"
-    await setDoc(doc(db, classificationsCollection, encodeURIComponent(trackId)), trackMetadata);
-}
+export const storeTrackClassifications = async (
+  trackId: string,
+  trackMetadata: object
+) => {
+  console.log("Storing in firebase... " + trackId);
+  // Add a new document in collection "cities"
+  await setDoc(
+    doc(db, classificationsCollection, encodeURIComponent(trackId)),
+    trackMetadata
+  );
+};
 
 export const getTrackClasisfications = async (trackId: string) => {
-    const docRef = doc(db, classificationsCollection, encodeURIComponent(trackId));
-    const docSnap = await getDoc(docRef);
+  const docRef = doc(
+    db,
+    classificationsCollection,
+    encodeURIComponent(trackId)
+  );
+  const docSnap = await getDoc(docRef);
 
-    console.log(docSnap.data())
+  console.log(docSnap.data());
 
-    return docSnap.data();
-}
+  return docSnap.data();
+};
 
 export const getAllTrackClasisfications = async () => {
-    const querySnapshot = await getDocs(collection(db, classificationsCollection));
+  const querySnapshot = await getDocs(
+    collection(db, classificationsCollection)
+  );
 
-    const featureVecs: any = {}
-    querySnapshot.forEach((doc) => {
-        const { scores } = doc.data()
-        featureVecs[decodeURIComponent(doc.id)] = scores
-    });
+  const featureVecs: any = {};
+  querySnapshot.forEach((doc) => {
+    const { scores } = doc.data();
+    featureVecs[decodeURIComponent(doc.id)] = scores;
+  });
 
-    return featureVecs;
-}
+  return featureVecs;
+};
 
 export const addFavorite = async (ownerId: string, trackId: string) => {
-    const currFavs = getFavorites(ownerId)
-    if (!currFavs) {
-        await setDoc(doc(db, "favorites", encodeURIComponent(ownerId)), { trackId: [trackId] });
-    } else {
-        await updateDoc(doc(db, "favorites", encodeURIComponent(ownerId)), { trackId: arrayUnion(trackId) });
-    }
-
-}
+  const currFavs = await getFavorites(ownerId);
+  if (!currFavs || currFavs.length === 0) {
+    await setDoc(doc(db, "favorites", encodeURIComponent(ownerId)), {
+      trackId: [trackId],
+    });
+  } else {
+    await updateDoc(doc(db, "favorites", encodeURIComponent(ownerId)), {
+      trackId: arrayUnion(trackId),
+    });
+  }
+};
 
 export const getFavorites = async (ownerId: string) => {
-    const docRef = doc(db, "favorites", encodeURIComponent(ownerId));
-    const docSnap = await getDoc(docRef);
+  const docRef = doc(db, "favorites", encodeURIComponent(ownerId));
+  const docSnap = await getDoc(docRef);
 
-    console.log(docSnap.data())
-
-    if (!docSnap.data()) {
-        return []
-    } else {
-        return docSnap.data()?.trackId;
-    }
-}
+  if (!docSnap.data()) {
+    return [];
+  } else {
+    return docSnap.data()?.trackId;
+  }
+};
